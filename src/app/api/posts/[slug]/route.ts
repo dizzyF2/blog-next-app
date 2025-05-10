548dbfc7d6973de3b2886deb14c6ab3f7f53f367
DizzyF2 <ifaris.ifaris172@gmail.com>
2025-05-10 22:03:34 +0300

    add: added views feature

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -7,8 +7,9 @@ export const GET = async (req: Request, {params}) =>{
 
     try {
 
-        const post = await prisma.post.findUnique({
+        const post = await prisma.post.update({
             where:{slug},
+            data:{ views: { increment: 1 } },
             include: {user: true},
         })
 
