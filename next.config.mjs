03d88def8bea36545af6215501f4e53235d395ed
DizzyF2 <ifaris.ifaris172@gmail.com>
2025-05-14 14:28:19 +0300

    fix: ensure NEXTAUTH_SECRET env available during build and runtime process

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
     images:{
         remotePatterns:[
             {
